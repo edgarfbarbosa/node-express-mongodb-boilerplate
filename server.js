07af5cc9fced032bb062715c4e063a03a7c63440
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-07-03 16:54:56 -0300

    feat: adiciona rota POST para criação de usuário

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const jsend = require('jsend');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
-const mongoose = require('mongoose');
+const User = require('./models/user.model');
+
+const app = express();
+
+app.use(express.json());
 
 /**
  * Rota GET para obter a lista de usuários.
@@ -17,6 +21,23 @@ app.get('/api/v1/users', (req, res) => {
       email: 'edgarfbarbosa@outlook.com'
     })
   );
+});
+
+/**
+ * Rota POST para criar um novo usuário.
+ * @description Adiciona um novo usuário no banco de dados e retorna o usuário criado.
+ * @param {newUser} Objeto JSON contendo os dados do novo usuário.
+ * @returns {Object} Objeto JSON contendo o usuário recém-criado.
+ * @throws {Error} Mensagem de erro caso a criação do usuário falhe.
+ */
+app.post('/api/v1/users', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+
+    res.status(201).json(jsend.success(newUser));
+  } catch (error) {
+    res.status(400).json(jsend.fail(error.message));
+  }
 });
 
 mongoose
