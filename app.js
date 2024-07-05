d02245eddc3da70ac5df59e2dd1024ce3f587288
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-07-05 19:36:31 -0300

    feat: adiciona middleware morgan para registro de dados das requisições

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const userRouter = require('./routes/user.routes');
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/users', userRouter);
 
