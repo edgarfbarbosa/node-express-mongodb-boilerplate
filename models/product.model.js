a764c534e09f9b10f937a093c1bce005ef3610d0
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-07-27 11:11:52 -0300

    refactor: torna 'discountPercentage' obrigatório e define valores padrão para 'productDetails' e 'images'

--- models/product.model.js
+++ models/product.model.js
@@ -11,12 +11,14 @@ const productSchema = new mongoose.Schema({
     unique: true,
     trim: true
   },
+  collection: String,
   price: {
     type: Number,
     required: [true, 'O produto deve ter um preço']
   },
   discountPercentage: {
     type: Number,
+    required: [true, 'O produto deve ter um percentual de desconto'],
     min: [0, 'O desconto não pode ser negativo'],
     max: [100, 'O desconto não pode ser maior que 100']
   },
@@ -24,14 +26,19 @@ const productSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  productDetails: [String],
+  productDetails: {
+    type: [String],
+    default: undefined
+  },
   stock: {
     type: Number,
     required: [true, 'Informe o número disponível de venda para este produto'],
     min: [0, 'O estoque não pode ser negativo']
   },
-  collection: String,
-  images: [String]
+  images: {
+    type: [String],
+    default: undefined
+  }
 });
 
 const Product = mongoose.model('Product', productSchema);
