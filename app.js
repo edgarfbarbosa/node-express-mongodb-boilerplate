be8e202ecc820fb04eaa4dded02253d3b4d41b51
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-07-29 12:44:55 -0300

    feat: adiciona o middleware CORS no arquivo app.js para permitir requisições de diferentes origens

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const userRouter = require('./routes/user.routes');
 const productRouter = require('./routes/product.routes');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
